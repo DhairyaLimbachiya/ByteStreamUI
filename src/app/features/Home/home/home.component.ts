8362960d327d6cd08d5ab476d1e52c5321194a68
Dhairya Limbachiya <dhairyalimbachiya5@gmail.com>
2024-03-19 16:40:45 +0530

    search bar added for home page

--- src/app/features/Home/home/home.component.ts
+++ src/app/features/Home/home/home.component.ts
@@ -15,82 +15,97 @@ import { JobSeeker } from '../../JobSeeker/model/JobSeeker.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  vacancies?:Vacancy[];
-  vacancyDetail :Vacancy={} as Vacancy;
+  vacancies?: Vacancy[];
+  vacancyDetail: Vacancy = {} as Vacancy;
   //test:Vacancy={} as Vacancy;
-  employer:Employer={}as Employer;
-  jobseeker:JobSeeker={}as JobSeeker;
-  user?:User;
-  applyRequest:ApplyRequest={} as ApplyRequest;
-  profileMade?:boolean=true;
-constructor(private vacancyService:VacancyService,private employerService:EmployerService,private authService:AuthService,private homeservice:HomeService ,private router:Router,private toast:NgToastService,private jobseekerService :JobseekerService){}
-ngOnInit(): void {
-  this.vacancyService.getAllVacancy().subscribe({
-    next: (response) => {
-      this.vacancies = response
-    }
-  });
-
-  this.authService.user().subscribe({
-    next:(response)=>{
-    this.user=response;
-    }
-  })
-
-}
-
-OnModelOpen(name:string,id:string){
-  this.employerService.getcomapnyByName(name).subscribe({
-    next:(response)=>{
-      this.employer=response;
-    }
-  }),
-  this.vacancyService.getvacancyById(id).subscribe({
-    next:(data)=>{
-      this.vacancyDetail = this.vacancies?.find((x) => x.id == id) || {} as Vacancy;
-   this.vacancyDetail.alreadyApplied=data.alreadyApplied
-
-    }
-  });
-
-    
-}
-
-ApplyVacancy(){
-this.applyRequest.vacancyId=this.vacancyDetail.id;
-if(this.user?.id){
- this.applyRequest.userId= this.user.id;
-
- this.applyRequest.appliedDate=new Date();
- this.jobseekerService.getJobSeeker(this.applyRequest.userId).subscribe({
-  next:(response)=>{
-    this.homeservice.apply(this.applyRequest).subscribe({
-      next:(ApplyRequest)=>{
-        this.toast.success({detail:"Success",summary:'Applied Successfully'})
-        console.log("profile is made");
-      }
-        });
+  employer: Employer = {} as Employer;
+  jobseeker: JobSeeker = {} as JobSeeker;
+  user?: User;
+  applyRequest: ApplyRequest = {} as ApplyRequest;
+  profileMade?: boolean = true;
+  allvacancies?: Vacancy[];
+  constructor(
+    private vacancyService: VacancyService,
+    private employerService: EmployerService,
+    private authService: AuthService,
+    private homeservice: HomeService,
+    private router: Router,
+    private toast: NgToastService,
+    private jobseekerService: JobseekerService
+  ) {}
+  ngOnInit(): void {
+    this.vacancyService.getAllVacancy().subscribe({
+      next: (response) => {
+        this.vacancies = response;
+         this.allvacancies=response;
       },
-  error:(error)=>{
-    this.profileMade=false;
-    if(!this.profileMade){
- this.toast.warning({detail:"Error",summary:'Kindly create your profile before applying'})
- this.router.navigateByUrl('/Jobseeker/jobseeker/add')
- 
-    }
+    });
+
+    this.authService.user().subscribe({
+      next: (response) => {
+        this.user = response;
+      },
+    });
   }
 
-}); 
+  OnModelOpen(name: string, id: string) {
+    this.employerService.getcomapnyByName(name).subscribe({
+      next: (response) => {
+        this.employer = response;
+      },
+    }),
+      this.vacancyService.getvacancyById(id).subscribe({
+        next: (data) => {
+          this.vacancyDetail =
+            this.vacancies?.find((x) => x.id == id) || ({} as Vacancy);
+          this.vacancyDetail.alreadyApplied = data.alreadyApplied;
+        },
+      });
+  }
 
+  ApplyVacancy() {
+    this.applyRequest.vacancyId = this.vacancyDetail.id;
+    if (this.user?.id) {
+      this.applyRequest.userId = this.user.id;
 
-
-
-}
-}
-
-
-
+      this.applyRequest.appliedDate = new Date();
+      this.jobseekerService.getJobSeeker(this.applyRequest.userId).subscribe({
+        next: (response) => {
+          this.homeservice.apply(this.applyRequest).subscribe({
+            next: (ApplyRequest) => {
+              this.toast.success({
+                detail: 'Success',
+                summary: 'Applied Successfully',
+              });
+              console.log('profile is made');
+            },
+          });
+        },
+        error: (error) => {
+          this.profileMade = false;
+          if (!this.profileMade) {
+            this.toast.warning({
+              detail: 'Error',
+              summary: 'Kindly create your profile before applying',
+            });
+            this.router.navigateByUrl('/Jobseeker/jobseeker/add');
+          }
+        },
+      });
+    }
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.vacancies = this.allvacancies;
+      return;
+    }
+    
+    this.vacancies = this.vacancies?.filter(vacancy => 
+      vacancy.jobTitle.toLowerCase().includes(text.toLowerCase()) ||
+      vacancy.publishedBy.toLowerCase().includes(text.toLowerCase())
+    );
+  }
 }
